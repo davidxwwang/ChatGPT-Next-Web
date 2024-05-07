dfba4206878337aa62b89d531208f69509d167ec
davidxwwang <xwwang_0102@qq.com>
2024-05-07 10:31:39 +0800

    改个标题，小试牛刀

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,11 +155,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Gpluse-Chat
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>AI与汽车电子的融合.</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
